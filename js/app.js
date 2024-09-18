5689bac4ca21098e6c07e0645f32641880bd5f66
Evangelina Vega <evangelina1001@hotmail.com>
2024-09-17 17:00:10 -0700

    created calculator class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 class Calculator {
+    constructor(result){
+        this.result = result
 
+        //binding DOM elements
+        this.operation = document.getElementById("operation")
+        this.num1 = document.getElementById("num1")
+        this.num2 = document.getElementById("num2")
+        this.calculate = document.getElementById("calculateBtn")
+
+
+        this.calculate.addEventListener('click', () => this.calculate())
+    }
+
+    calculate(){
+        const option = this.operation.value
+
+        if(option === "+"){
+            return this.num1 + this.num2
+        }else if(option === "-"){
+            return this.num1 - this.num2
+        }else if(option === "/"){
+            return this.num1 / this.num2
+        }else if(option === "*"){
+            return this.num1 * this.num2
+        }
+    }
 }
+
